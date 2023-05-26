8ae3b72129815e8939f7042dcadc053996fd8ae0
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2023-05-26 18:44:23 +0300

    Create testvpc.tf

---
+++ testvpc.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2"  # update with your desired region
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "TestVPC"
+  }
+}
+
+resource "aws_subnet" "test_subnet" {
+  vpc_id     = aws_vpc.test_vpc.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "TestSubnet"
+  }
+}
